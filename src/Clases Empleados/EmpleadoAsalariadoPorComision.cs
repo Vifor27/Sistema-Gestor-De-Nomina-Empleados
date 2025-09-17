f6d8f580f09c901568ce9f52f2192e0bcdad8f1a
Vifor27 <156149450+Vifor27@users.noreply.github.com>
2025-09-16 21:45:01 -0400

    Add files via upload

---
+++ src/Clases Empleados/EmpleadoAsalariadoPorComision.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Nomina.Clases_Empleados
+{
+    // Clase hija empleado asalariado por comision, hereda de clase padre EmpleadoPorComision
+    internal class EmpleadoAsalariadoPorComision : EmpleadoPorComision
+    {
+        // Propiedades
+        public decimal salarioBase { get; set; }
+
+        // Consctructor
+        public EmpleadoAsalariadoPorComision(string primerNombre, string apellidoPaterno, string nss, decimal ventasBrutas, decimal tarifaComision, decimal salarioBase)
+                      : base(primerNombre, apellidoPaterno, nss, ventasBrutas, tarifaComision)
+        {
+            this.salarioBase = salarioBase;
+        }
+
+        // Metodos sobrescritos
+        public override decimal CalcularPagoSemanal()
+        {
+            // Salario base mas comisiones
+            return salarioBase + (ventasBrutas * tarifaComision);
+        }
+
+        public override string GenerarDetalle()
+        {
+           return $"Asalariado mas comision: {primerNombre} {primerApellido} | NSS: {numeroSeguroSocial} | Ventas: {ventasBrutas:C} | Base: {salarioBase:C} | Pago: {CalcularPagoSemanal():C}";
+        }
+    }
+}
