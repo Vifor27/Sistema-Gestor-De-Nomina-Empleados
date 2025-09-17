c779775c6c3ed5b5a9224151f17a99161d90eb33
Vifor27 <156149450+Vifor27@users.noreply.github.com>
2025-09-16 22:16:34 -0400

    Add files via upload

---
+++ src/Clases Empleados/Empleado.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Nomina
+{   
+    // Clase abstracta base para Empleado
+    public abstract class Empleado
+    {
+        // Propiedades
+        public string primerNombre { get; set; }
+        public string primerApellido { get; set; }
+        public string numeroSeguroSocial { get; set; }
+
+        // Constructor
+        protected Empleado (String primerNombre, String primerApellido, string numeroSeguroSocial)
+        {
+            this.primerNombre = primerNombre;
+            this.primerApellido = primerApellido;
+            this.numeroSeguroSocial = numeroSeguroSocial;
+        }
+
+        // Metodos base
+        public abstract decimal CalcularPagoSemanal();
+        public abstract string GenerarDetalle();
+
+    }
+
+
+}
---
+++ src/Clases Empleados/EmpleadoAsalariado.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Nomina
+{
+    // Clase empleado asalariado, hereda de clase padre empleado
+    public class EmpleadoAsalariado : Empleado
+    {
+        // Propiedades
+        public decimal salarioSemanal { get; set; }
+
+        // Constructor
+        public EmpleadoAsalariado (string primerNombre, string primerApellido, string numeroSeguroSocial, decimal salarioSemanal) 
+            : base (primerNombre, primerApellido, numeroSeguroSocial)
+        {
+            this.salarioSemanal = salarioSemanal;
+        }
+
+        // Metodos sobrescritos
+        public override decimal CalcularPagoSemanal()
+        {
+            // El empleado asalariado siempre cobra lo mismo
+            return salarioSemanal;
+        }
+
+        public override string GenerarDetalle()
+        {
+            return $"-- Asalariado: {primerNombre} {primerApellido} | NSS: {numeroSeguroSocial} | Pago: {CalcularPagoSemanal():C}";
+        }
+
+
+    }
+}
